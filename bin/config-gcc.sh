46de59b7398da4b768a34de71272f8422e8a3f66
Dave Abrahams <dave@boostpro.com>
2013-01-13 12:14:23 -0500

    Delete apparently-obsolete scripts

--- bin/config-gcc.sh
+++
@@ -1,1 +0,0 @@
-./configure --prefix=/usr/local/stow/subconvert CC=gcc-mp-4.8 LD=g++-mp-4.8 CXX=g++-mp-4.8 CPPFLAGS="-isystem /opt/local/include -isystem /usr/local/include" LDFLAGS="-L/opt/local/lib -L/usr/local/lib"
--- bin/patch-config.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-perl -i -pe 's%url = git://github.com/boost-lib%url = git\@github.com:boost-lib%;' \
-    $1/**/.git/config
