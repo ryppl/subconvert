c4a8991c50fbab47e6b8b0cacda49f4c66aa327c
Dave Abrahams <dave@boostpro.com>
2013-01-13 21:35:51 -0500

    Hoping to get more useful information, flush the error stream in a few key places

--- src/status.h
+++ src/status.h
@@ -74,7 +74,7 @@ public:
   void debug(const std::string& message) const {
     if (debug_mode()) {
       newline();
-      out << "r" << rev << ": " << message << std::endl;
+      out << "r" << rev << ": " << message << std::endl << std::flush;
       need_newline = false;
     }
   }
@@ -102,7 +102,7 @@ public:
   void error(const std::string& message) const {
     ++errors;
     newline();
-    out << "r" << rev << ": Error: " << message << std::endl;
+    out << "r" << rev << ": Error: " << message << std::endl << std::flush;
     need_newline = false;
   }
 
